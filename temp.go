b6ae99bdc0c94a8c1cdd20a6f2f38a332a6f2826
dawei888 <zhaoyiwei@prodigy.local>
2015-07-31 17:27:15 +0800

    update

--- temp.go
+++ temp.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
-func main() {
-	// var arr1 [5]int
-	arr1 := [5]int{}
-	for i := 0; i < len(arr1); i++ {
-		arr1[i] = i * 2
-		fmt.Printf("Array at index %d is %d\n", i, arr1[i])
-	}
+type Integer int
 
+func (a *Integer) Add(b Integer) {
+	*a += b
+}
+
+func main() {
+	var a Integer = 1
+	a.Add(2)
+	fmt.Println("a =", a)
 }
